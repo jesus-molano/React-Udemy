fa16014d71403763c17e8d8fd5f83ed693a4ad35
jesus-molano <jessumolano@gmail.com>
2021-10-21 14:41:22 +0100

    Directory name changed

--- 09-calendar-app/src/components/CalendarModal/customStyles.js
+++
@@ -1,7 +0,0 @@
-export const customStyles = {
-  content: {
-    // top: '50%',
-    // left: '50%',
-    // transform: 'translate(-50%, -50%)',
-  }
-}
--- 09-calendar-app/src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import CalendarApp from './CalendarApp'
-import './styles/styles.scss'
-
-ReactDOM.render(
-    <CalendarApp />,
-  document.getElementById('root')
-)
